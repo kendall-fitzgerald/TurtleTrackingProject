8db9031324b8706bc276fd679529983432116f22
kendall_fitzgerald <kendall.fitzgerald@duke.edu>
2025-09-22 12:52:43 -0400

    Tracking Tool in progress

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -8,3 +8,23 @@
 # Author: Kendall Fitzgerald (kendall.fitzgerald@duke.edu)
 # Date:   Fall 2025
 #--------------------------------------------------------------
+
+# Parse Data
+# Copy and paste a line of data as the lineString variable value
+lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
+  
+# Use the split command to parse the items in lineString into a list object
+lineData = lineString.split()
+print(lineData)
+  
+# Assign variables to specfic items in the list
+record_id = lineData[0]   # ARGOS tracking record ID
+print(record_id)
+obs_date = lineData[2]   # Observation date
+ob_lc = lineData[3]       # Observation Location Class
+obs_lat = lineData[5,7]  # Observation Latitude
+print(obs_lat)   
+obs_lon = lineData[6,8]     # Observation Longitude
+  
+# Print information to the use
+print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
